0c33e4c6f6cf280877ecc37a5f61aecd900c8244
wantedwriter <github@wezlab.net>
2023-12-12 21:41:18 -0600

    Update EA-Nudge-Logs.sh
    Added 14* for Sonoma

--- EA-Nudge-Logs.sh
+++ EA-Nudge-Logs.sh
@@ -11,7 +11,7 @@ case "${osProductVersion}" in
 			echo "<result>N/A; macOS ${osProductVersion}</result>"
 			;;
 
-	11* | 12* | 13* )
+	11* | 12* | 13* | 14* )
 			loggedInUser=$( /bin/echo "show State:/Users/ConsoleUser" | /usr/sbin/scutil | /usr/bin/awk '/Name :/ { print $3 }' )
 			requiredOS=$( /usr/bin/defaults read /Users/"${loggedInUser}"/Library/Preferences/com.github.macadmins.Nudge.plist requiredMinimumOSVersion )
 			logEntries=$( /usr/bin/grep com.github.macadmins.Nudge /var/log/Nudge.log | /usr/bin/tail -5 | /usr/bin/awk '{ split($2, split_time, "."); $2 = split_time[1]; print }' | /usr/bin/cut -d ' ' -f -2,6- )
